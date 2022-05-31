c996cce82a481bb0bcb5c3113b5c6b2549646f5e
User-X <User-X@DESKTOP-SQQ81L5>
2022-05-31 16:36:20 +0300

    Добавьте файлы проекта.

---
+++ TermPaper2022.cpp
@@ -0,0 +1,36 @@
+﻿#include <string>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class BaseTarget
+{
+protected:
+    const int rows;
+    const int cols;
+
+public:
+    BaseTarget() :rows(0), cols(0) {}
+    BaseTarget(int rows, int cols) :rows(rows), cols(cols) 
+    {
+
+    }
+    
+
+  virtual  int CenterCoordinates()
+    {
+
+    }
+   virtual int Shot(int x,int y)
+    {
+
+    }
+        
+
+};
+
+int main()
+{
+   
+}
